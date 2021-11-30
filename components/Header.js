c8f091e1b6567a7a507bd961a195354fb8a5be2e
Joseph Mangubat <josephmangubat@josephs-mini.mynet>
2021-11-30 21:47:56 +0000

    pyNotes

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,27 @@
 import React from "react";
 // import Image from "next/image";
-import { BellIcon, ChatIcon } from "@heroicons/react/solid";
-import { FlagIcon, SearchIcon, GlobeIcon } from "@heroicons/react/outline";
+import {
+  BellIcon,
+  ChatIcon,
+  HomeIcon,
+  ShoppingCartIcon,
+  UserGroupIcon,
+} from "@heroicons/react/solid";
+import {
+  FlagIcon,
+  SearchIcon,
+  GlobeIcon,
+  PlayIcon,
+} from "@heroicons/react/outline";
+import HeaderIcon from "./HeaderIcon";
 
 function Header() {
   return (
-    <div>
-      <h1>Header</h1>
+    <div
+      className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5
+    shadow-md"
+    >
+      <h1>pyNotes</h1>
       {/* Left */}
       <div className="flex items-center">
         {/* <Image
@@ -15,14 +30,32 @@ function Header() {
           height={40}
           layout="fixed"
         /> */}
-        <GlobeIcon className="h-6" />
-        <div className="flex">
-          <SearchIcon className="h-6" />
-          <input type="text" placeholder="Search pyNotes" />
+        <GlobeIcon className="h-6 " />
+        <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
+          <SearchIcon className="h-6 text-gray-600" />
+          <input
+            className="hidden md:inline-flex  ml-2 items-center bg-transparent outline-none 
+          placeholder-gray-500 flex-shrink"
+            type="text"
+            placeholder="Search pyNotes"
+          />
         </div>
       </div>
       {/* Center */}
+      <div className=" flex justify-center flex-grow">
+        <div className="flex space-x-6 md:space-x-2">
+          <HeaderIcon active Icon={HomeIcon} />
+          <HeaderIcon Icon={FlagIcon} />
+          <HeaderIcon Icon={PlayIcon} />
+          <HeaderIcon Icon={ShoppingCartIcon} />
+          <HeaderIcon Icon={UserGroupIcon} />
+        </div>
+      </div>
       {/* Right */}
+      <div className="flex items-center sm:space-x-2 justify-end">
+        {/* Profile Pic */}
+        <p className="whitespace-nowrap font-semibold pr-3">User Name</p>
+      </div>
     </div>
   );
 }
